ee93ddef3681f3e4bc4da5706e57cf925d052806
Kevin van Hulst <kevin.van.hulst@gmail.com>
2015-11-26 13:56:52 +0100

    Added empty project

---
+++ src/Lisa.Excelsis.Mobile/Lisa.Excelsis.Mobile/App.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.Forms;
+
+namespace Lisa.Excelsis.Mobile
+{
+    public class App : Application
+    {
+        public App()
+        {
+            MainPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        new Label {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = "Welcome to Xamarin Forms!"
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
