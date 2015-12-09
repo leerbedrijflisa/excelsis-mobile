4824882e00209a5262d664a4989ab382e640d7f4
Kevin van Hulst <kevin.van.hulst@gmail.com>
2015-12-09 08:39:26 +0100

    Changed the database class to a generic database class

--- src/Lisa.Excelsis.Mobile/Lisa.Excelsis.Mobile.Droid/Resources/Resource.Designer.cs
+++ src/Lisa.Excelsis.Mobile/Lisa.Excelsis.Mobile.Droid/Resources/Resource.Designer.cs
@@ -26,6 +26,8 @@ namespace Lisa.Excelsis.Mobile.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::SQLite.Net.Platform.XamarinAndroid.Resource.String.ApplicationName = global::Lisa.Excelsis.Mobile.Droid.Resource.String.ApplicationName;
+			global::SQLite.Net.Platform.XamarinAndroid.Resource.String.Hello = global::Lisa.Excelsis.Mobile.Droid.Resource.String.Hello;
 			global::Xamarin.Forms.Platform.Resource.Animation.abc_fade_in = global::Lisa.Excelsis.Mobile.Droid.Resource.Animation.abc_fade_in;
 			global::Xamarin.Forms.Platform.Resource.Animation.abc_fade_out = global::Lisa.Excelsis.Mobile.Droid.Resource.Animation.abc_fade_out;
 			global::Xamarin.Forms.Platform.Resource.Animation.abc_grow_fade_in_from_bottom = global::Lisa.Excelsis.Mobile.Droid.Resource.Animation.abc_grow_fade_in_from_bottom;
--- src/Lisa.Excelsis.Mobile/Lisa.Excelsis.Mobile/Data/ExamDatabase.cs
+++ src/Lisa.Excelsis.Mobile/Lisa.Excelsis.Mobile/Data/ExamDatabase.cs
@@ -3,11 +3,12 @@ using Xamarin.Forms;
 
 namespace Lisa.Excelsis.Mobile
 {
-    public class ExamDatabase
+    public class DataBase<T> where T : class
     {
-        public ExamDatabase()
+        public DataBase()
         {
             _database = DependencyService.Get<ISQLite>().GetConnection();
+            _database.CreateTable<T>();
         }
 
         private SQLiteConnection _database;
