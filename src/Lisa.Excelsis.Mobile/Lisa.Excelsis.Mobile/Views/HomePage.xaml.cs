e67d746abc3beed1463d42eb4084c237eb284b46
Patrick Huiskens <patrickhuiskens@gmail.com>
2016-02-04 11:01:23 +0100

    fixed text in ticket15

--- src/Lisa.Excelsis.Mobile/Lisa.Excelsis.Mobile/Views/HomePage.xaml.cs
+++ src/Lisa.Excelsis.Mobile/Lisa.Excelsis.Mobile/Views/HomePage.xaml.cs
@@ -44,7 +44,7 @@ namespace Lisa.Excelsis.Mobile
 
                 ErrorList.ItemsSource = new List<string>()
                 {
-                    "Error boys"
+                    "Geen examens gevonden, pull to refresh."
                    
                 };
 
