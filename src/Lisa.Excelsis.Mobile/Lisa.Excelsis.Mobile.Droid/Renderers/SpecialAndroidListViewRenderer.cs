bc2ee3b6a6257683894970e211aa7d8217a69b84
Jim Geersinga <j.geersinga@outlook.com>
2016-02-24 10:12:18 +0100

    added custom renderers

---
+++ src/Lisa.Excelsis.Mobile/Lisa.Excelsis.Mobile.Droid/Renderers/SpecialAndroidListViewRenderer.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer (typeof(NativeListView), typeof(SpecialAndroidListViewRenderer))]
+namespace Lisa.Excelsis.Mobile.Droid
+{
+	public class SpecialAndroidListViewRenderer : ListViewRenderer
+	{
+		protected override void OnElementChanged (ElementChangedEventArgs<Xamarin.Forms.ListView> e)
+		{
+			base.OnElementChanged (e);
+
+			if (e.OldElement != null) {
+				// unsubscribe
+				Control.ItemClick -= OnItemClick;
+			}
+
+			if (e.NewElement != null) {
+				// subscribe
+				Control.Adapter = new NativeAndroidListViewAdapter (Forms.Context as Android.App.Activity, e.NewElement as NativeListView);
+				Control.ItemClick += OnItemClick;
+			}
+		}
+
+		void OnItemClick (object sender, Android.Widget.AdapterView.ItemClickEventArgs e)
+		{           
+			((NativeListView)Element).NotifyItemSelected (((NativeListView)Element).Items.ToList () [e.Position - 1]);
+		}
+	}
+}
---
+++ src/Lisa.Excelsis.Mobile/Lisa.Excelsis.Mobile/Renderers/SpecialListView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Xamarin.Forms;
+using System.Collections.Generic;
+
+namespace Lisa.Excelsis.Mobile
+{
+	public class SpecialListView : ListView
+	{
+		public static readonly BindableProperty ItemsProperty =
+			BindableProperty.Create ("Items", typeof(IEnumerable<DataSource>), typeof(SpecialListView), new List<DataSource> ());
+
+		public IEnumerable<DataSource> Items {
+			get { return (IEnumerable<DataSource>)GetValue (ItemsProperty); }
+			set { SetValue (ItemsProperty, value); }
+		}
+
+		public event EventHandler<SelectedItemChangedEventArgs> ItemSelected;
+
+		public void NotifyItemSelected (object item)
+		{
+			if (ItemSelected != null) {
+				ItemSelected (this, new SelectedItemChangedEventArgs (item));
+			}
+		}
+	}
+}
+
+
