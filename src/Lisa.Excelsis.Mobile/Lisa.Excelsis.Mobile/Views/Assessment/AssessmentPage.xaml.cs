31ce01775d6ebae3393af0cffdf6e0214632d809
William Willing <william@williamwilling.com>
2016-03-17 10:38:43 +0100

    Revert "Added IOS icons"
    This reverts commit e5c0d8cc567bc4116560e19156a371a0bf0895f6.

--- src/Lisa.Excelsis.Mobile/Lisa.Excelsis.Mobile/Views/Assessment/AssessmentPage.xaml.cs
+++ src/Lisa.Excelsis.Mobile/Lisa.Excelsis.Mobile/Views/Assessment/AssessmentPage.xaml.cs
@@ -64,10 +64,12 @@ namespace Lisa.Excelsis.Mobile
                 if( oldItem != null && item.Id == oldItem.Id && item.IsSelected)
                 {
                     item.IsSelected = false;
+                    ObservationCell.ForceUpdateSize();
                     CategoryList.SelectedItem = null;
                 }   
                 else if( oldItem != null && item.Id == oldItem.Id && !item.IsSelected)
                 {
+                    ObservationCell.ForceUpdateSize();
                     item.IsSelected = true;
                 }
                 else if (oldItem != null && item.Id != oldItem.Id)
@@ -75,16 +77,16 @@ namespace Lisa.Excelsis.Mobile
                     oldItem.IsSelected = false;
                     item.IsSelected = true;
                     oldItem = item;
+                    ObservationCell.ForceUpdateSize();
                     CategoryList.SelectedItem = item;
                 }    
                 else
                 {
                     item.IsSelected = true;
                     oldItem = item;
+                    ObservationCell.ForceUpdateSize();
                     CategoryList.SelectedItem = item;
                 }
-
-                ((ListView)sender).FindByName<ViewCell>("ObservationCell").ForceUpdateSize();
             };           
         }
 
