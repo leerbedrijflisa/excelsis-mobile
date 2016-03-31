cd9ba7ca37e2971fb9213b08578d1a39c66456c0
Joost Ronkes Agerbeek <joost@Joosts-MacBook-Pro.local>
2016-03-31 10:51:41 +0200

    fixed ios roundedbox

--- src/Lisa.Excelsis.Mobile/Lisa.Excelsis.Mobile.iOS/RoundedBoxRenderer.cs
+++ src/Lisa.Excelsis.Mobile/Lisa.Excelsis.Mobile.iOS/RoundedBoxRenderer.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using Xamarin.Forms.Platform.IOS;
-using MonoTouch.UiKit;
-using MonoTouch.CoreGraphics;
 using Xamarin.Forms;
 using Lisa.Excelsis.Mobile;
 using Lisa.Excelsis.Mobile.iOS;
+using Xamarin.Forms.Platform.iOS;
+using UIKit;
+using CoreGraphics;
 
-[assembly: ExportRendererAttribute(typeof(RoundedBoxView), typeof(RoundedBoxViewRenderer))]
+[assembly: ExportRendererAttribute(typeof(RoundedBoxView), typeof(RoundedBoxRenderer))]
 namespace Lisa.Excelsis.Mobile.iOS
 {
     public class RoundedBoxRenderer : BoxRenderer
@@ -15,7 +15,7 @@ namespace Lisa.Excelsis.Mobile.iOS
         {
         }
 
-        public override void Draw(System.Drawing.RectangleF rect)
+		public override void Draw (CoreGraphics.CGRect rect)
         {
             RoundedBoxView rbv = (RoundedBoxView)this.Element;
 
@@ -28,7 +28,7 @@ namespace Lisa.Excelsis.Mobile.iOS
                 var rc = this.Bounds.Inset((int)rbv.StrokeThickness, (int)rbv.StrokeThickness);
 
                 float radius = (float)rbv.CornerRadius;
-                radius = Math.Max(0, Math.Min(radius, Math.Max(rc.Height / 2, rc.Width / 2)));
+				radius = (float)Math.Max(0, Math.Min(radius, Math.Max(rc.Height / 2, rc.Width / 2)));
 
                 var path = CGPath.FromRoundedRect(rc, radius, radius);
                 context.AddPath(path);
